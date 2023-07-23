152fc2c7c8e4e8b777554912dbc1762a44e24231
Carlos Buitrago <letscatchupagain@gmail.com>
2023-07-23 21:59:53 +0100

    files added

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,80 @@
-function assertEquals(expect, actual) {}
+/* eslint-disable no-plusplus */
+function assertEquals(expect, actual) {
+  // handles strings
+  if (typeof expect === 'string' && typeof actual === 'string') {
+    if (actual === '') {
+      throw new Error(`expected ${expect} but found an empty string`);
+    }
+    if (actual.length > '30') {
+      throw new Error(`String exceeds length limit (max. 30 characters)`);
+    }
+    if (expect !== actual.toLowerCase().trim()) {
+      throw new Error(`expected ${expect} but found ${actual.trim()}`);
+    }
+    return 'No error';
+  }
 
-module.exports = assertEquals
+  // handles numbers
+  if (typeof expect === 'number' && typeof actual === 'number') {
+    if (expect !== actual) {
+      throw new Error(`expected ${expect} but found ${actual}`);
+    }
+    return 'No error';
+  }
+
+  // handles different data types
+  if (typeof expect !== typeof actual) {
+    throw new Error(
+      `Expected type ${typeof expect} but found type ${Object.prototype.toString.call(
+        actual
+      )}`
+    );
+  }
+
+  // handles arrays
+  if (Array.isArray(expect) && Array.isArray(actual)) {
+    const actualFiltered = actual
+      .map((item) => {
+        if (typeof item === 'string') {
+          return item.trim().toLowerCase();
+        }
+        return item;
+      })
+      .filter((item) => item !== undefined);
+
+    const expectCaseInsensitive = expect.map((item) => {
+      if (typeof item === 'string') {
+        return item.toLowerCase();
+      }
+      return item;
+    });
+    // handles non-matching lengths of array
+    if (expect.length !== actualFiltered.length) {
+      throw new Error(
+        `expected array length ${expect.length} but found ${actualFiltered.length}`
+      );
+    }
+    // handles different types of entries in array
+    for (let i = 0; i < expect.length; i++) {
+      if (typeof expect[i] !== typeof actualFiltered[i]) {
+        throw new Error(
+          `expected ${typeof expect[i]} at index ${
+            actualFiltered[i]
+          }, but found ${typeof actualFiltered[i]}`
+        );
+      }
+      // handles non-mathcing entries in array
+      if (expectCaseInsensitive[i] !== actualFiltered[i]) {
+        throw new Error(
+          `expected ${expect[i]} at index ${actualFiltered.indexOf(
+            actualFiltered[i]
+          )}, but found ${actualFiltered[i]}`
+        );
+      }
+    }
+    // handles matching arrays
+    return 'No error';
+  }
+}
+
+module.exports = assertEquals;
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,80 @@
-const assertEquals = require('./assert-equals')
+/* eslint-disable no-sparse-arrays */
+const assertEquals = require('./assert-equals');
 
 describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
-  })
+  describe('When expected and actual are the same string:', () => {
+    it('Returns without throwing an error.', () => {
+      expect(() => assertEquals('abc', 'abc')).not.toThrow();
+      expect(() => assertEquals(1, 1)).not.toThrow();
+    });
+    it('Removes whitespaces.', () => {
+      expect(() => assertEquals('abc', ' abc  ')).not.toThrow();
+    });
+    it('Performs a case-insensitive string comparison.', () => {
+      expect(() => assertEquals('abc', 'ABC')).not.toThrow();
+    });
+  });
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
-  })
-})
+  describe('When expected and actual are different strings:', () => {
+    it('Throws an error when strings are not the same.', () => {
+      expect(() => assertEquals('abc', 'acd')).toThrow();
+      expect(() => assertEquals(1, 2)).toThrow();
+      expect(() => assertEquals(1, '1')).toThrow();
+    });
+    it('Thows descriptive error when actual is an empty string.', () => {
+      expect(() => assertEquals('a', '')).toThrow();
+    });
+    it('Thows an error when string is too long, e.g.: >30 characters', () => {
+      expect(() =>
+        assertEquals('hello', 'helloooooooooooooooooooooooooooooo')
+      ).toThrow();
+    });
+  });
+
+  describe('When expected and actual are different data types:', () => {
+    it('Throws an error when indicating the internal Class property.', () => {
+      expect(() => assertEquals('a,b,c', ['a', 'b', 'c'])).toThrow();
+    });
+  });
+
+  describe('When expected and actual are the same array:', () => {
+    it('Returns without throwing an error.', () => {
+      expect(() =>
+        assertEquals(['a', 'b', 'c'], ['a', 'b', 'c'])
+      ).not.toThrow();
+    });
+    it('Performs a case-insensitive array comparison.', () => {
+      expect(() =>
+        assertEquals(['North', 'Oak', 'Bank'], ['NORTH', 'oak', 'BANK'])
+      ).not.toThrow();
+    });
+    it('Handles arrays of numbers.', () => {
+      expect(() => assertEquals([1, 2, 3], [1, 2, 3])).not.toThrow();
+    });
+    it('Handles arrays of numbers and strings.', () => {
+      expect(() => assertEquals([1, 2, 'a'], [1, 2, 'a'])).not.toThrow();
+    });
+    it('Empty slots are not considered.', () => {
+      expect(() =>
+        assertEquals(['a', 'b', 'c'], [, , 'a', , 'b', , 'c', ,])
+      ).not.toThrow();
+    });
+    it('Strings within arrays with whitespaces pass the test.', () => {
+      expect(() =>
+        assertEquals(['a', 'b', 'c'][('a', '  b', 'c  ')])
+      ).not.toThrow();
+    });
+  });
+
+  describe('When expected and actual are different arrays:', () => {
+    it('Throws an error when arrays have different length.', () => {
+      expect(() => assertEquals(['a', 'b'], ['a', 'b', 'c'])).toThrow();
+    });
+    it('Throws an error when items in the array are different.', () => {
+      expect(() => assertEquals(['a', 'b', 'c'], ['a', 'c', 'c'])).toThrow();
+    });
+    it('Handles correct error when item in array is of different type.', () => {
+      expect(() => assertEquals([1, 2, 3], [1, '2', 3])).toThrow();
+    });
+  });
+});
